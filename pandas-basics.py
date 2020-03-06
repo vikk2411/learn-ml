a3aed325f78d34661a9ef1ab37f8b1e6e775110c
Vikas Verma <vikk2411@gmail.com>
2020-03-06 10:04:35 +0530

    added some pandas code

---
+++ pandas-basics.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+s1 = pd.Series([10, 20 , 30])
+# print("-----s1 ------")
+# print(s1)
+
+s2 = pd.Series([10, 20 , 30], index = ['a', 'b', 'c'])
+# print("-----s2 ------")
+# print(s2)
+
+dic1 = { 'a': 50, 'b': 60, 'c': 70}
+s3 = pd.Series(dic1)
+# print("-----s3 ------")
+# print(s3)
+
+report = pd.DataFrame({ "Name": ["Same", "Alice", "Mark"], "marks": [78, 92, 49]  })
+print(report)
+print(report.shape)
